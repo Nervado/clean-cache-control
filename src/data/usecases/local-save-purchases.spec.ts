b190f3a50581ffb5feee75086f7818769a9263db
Nervado <evandroabreudeoliveira@gmail.com>
2020-12-26 17:58:47 -0300

    ensure LocalSavePurchase delete cache with given key

--- src/data/usecases/local-save-purchases.spec.ts
+++ src/data/usecases/local-save-purchases.spec.ts
@@ -2,18 +2,19 @@ class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore){}
 
   async save (): Promise<void> {
-    this.cacheStore.delete();
+    this.cacheStore.delete('purchases');
   }
 }
 
 interface CacheStore {
-  delete: () => void
+  delete: (key:string) => void
 
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
-  delete (): void {
+  key = 'purchases'
+  delete (key:string): void {
      this.deleteCallsCount++
   }
 }
@@ -42,6 +43,12 @@ describe('LocalSavePurchase', () => {
     const { cacheStore, sut } = makeSut()    
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
-    })  
+    }) 
+
+  test('Should call delete with correct key', async () => {
+      const { cacheStore, sut } = makeSut()    
+      await sut.save()
+      expect(cacheStore.key).toBe('purchases')
+      })  
 });
 
